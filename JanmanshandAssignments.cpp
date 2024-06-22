3f1b417e716ca25e7e7b1d6c3dc3d9723e47a2e1
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-06-22 11:50:09 +0530

    initial commit

---
+++ JanmanshandAssignments.cpp
@@ -0,0 +1,21 @@
+/*
+CodeChef: Janmansh and Assignments
+Link: https://www.codechef.com/problems/JASSIGNMENTS
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+	    cin>>x;
+	    if(x+3<=10)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+
+}
