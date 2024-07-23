aa98fe9914dfd8ba4c6fce52c9c20075df8a5287
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-07-23 14:25:18 +0530

    initial commit

---
+++ RomanToInteger.cpp
@@ -0,0 +1,30 @@
+/*
+Leetcode: 13. Roman to Integer
+Link: https://leetcode.com/problems/roman-to-integer/description/?envType=study-plan-v2&envId=top-interview-150
+*/
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(m[s[i]]<m[s[i+1]])
+            {
+                ans-=m[s[i]];
+            }
+            else
+            {
+                ans+=m[s[i]];
+            }
+        }
+    return ans;
+    }
+};
