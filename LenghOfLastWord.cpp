7c27c41cb91117a96f7e9c139bdce9da3a9802ab
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-07-23 23:01:40 +0530

    initial commit

--- LenghOfLastWord.cpp
+++ LenghOfLastWord.cpp
@@ -7,7 +7,7 @@ public:
     int lengthOfLastWord(string s) {
      int n=s.size();
      int c=0;
-     int i=n-1;
+     int i=n-1; //to update i value, so that when it exits the 'while loop', the updated value of i is taken for further execution in 'for loop'
      while(i>=0&&s[i]==' ')
      {
         i--;
