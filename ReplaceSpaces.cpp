6d7ed5537dc0ed1f91c2efd0a0567345169ddb6c
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-08-02 00:05:19 +0530

    initial commit

--- ReplaceSpaces.cpp
+++ ReplaceSpaces.cpp
@@ -1,19 +1,17 @@
-/*CodingNinjas- Replace Spaces
+/*
 Link: https://www.naukri.com/code360/problems/replace-spaces_1172172?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_5&leftPanelTabValue=PROBLEM
-
 */
 #include <bits/stdc++.h> 
 string replaceSpaces(string &str){
 	// Write your code here.
-    string res;
-    for(int i=0;i<str.size();i++)
-    {
-      if (str[i] == ' ') {
-        res.push_back('@');
-        res.push_back('4');
-        res.push_back('0');
-      } else
-        res.push_back(str[i]);
-    }
-    return res;
+	string add="@40";
+	for(int i=0;i<str.size();i++)
+	{
+		if(str[i]==' ')
+		{
+			str.replace(i,1,add); //ith position space which is a single space character to be replaced with add staring
+			i=i+3; //since length of 'add' is 3
+		}
+	}
+	return str;
 }
