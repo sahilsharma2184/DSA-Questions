a20c062e6b84a40c167a11c12c9693acca97f608
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-08-06 23:07:57 +0530

    2 solutions

--- BestDayToBuyAndSell.cpp
+++ BestDayToBuyAndSell.cpp
@@ -22,3 +22,28 @@ public:
         return profit;
     }
 };
+
+//2nd approach
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buycost=prices[0];
+        int profit=0;
+        if(prices.size()<2)
+        {
+            return 0;
+        }
+        for(int i=1;i<prices.size();i++)
+        {
+            if(buycost>prices[i]) //if you bought stock at higher price than the next day
+            {
+                buycost=prices[i]; 
+            }
+            else
+            {
+                profit=max(profit,prices[i]-buycost); 
+            }
+        }
+        return profit;
+    }
+};
